21e8d87fdb5f5ff68eb8a75c472be2e1b02008e2
Onur <onur05eroglu@gmail.com>
2025-11-03 20:11:44 +0200

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -123,12 +123,12 @@ if (useAppConfig)
         .ConfigureRefresh(refresh =>
         {
             // Default cache expiration is 30 seconds
-            refresh.Register("eShopWeb:Settings:NoResultsMessage").SetCacheExpiration(TimeSpan.FromSeconds(30));
+            options.SetRefreshInterval(TimeSpan.FromSeconds(30))
         })
         .UseFeatureFlags(featureFlagOptions =>
         {
             // Default cache expiration is 30 seconds
-            featureFlagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(30);
+            featureFlagOptions.SetRefreshInterval(TimeSpan.FromSeconds(30));
         });
     });
 }
